7bab681109894932addd115b6171cd7a666cd4a2
Jonathan Flores <jonathan.flores@mail.udp.cl>
2024-12-01 19:19:22 -0300

    Remove notification menu from header template

--- Views/Templates/header.php
+++ Views/Templates/header.php
@@ -33,18 +33,6 @@
         <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
         <!-- Navbar Right Menu-->
         <ul class="app-nav">
-            <!--Notification Menu-->
-            <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
-                <ul class="app-notification dropdown-menu dropdown-menu-right">
-                    <li class="app-notification__title">Libros no entregados.</li>
-                    <div class="app-notification__content">
-                        <li id="nombre_estudiante">
-                            
-                        </li>
-                    </div>
-                    <li class="app-notification__footer"><a href="<?php echo base_url; ?>Configuracion/libros" target="_blank">Generar Reporte.</a></li>
-                </ul>
-            </li>
             <!-- User Menu-->
             <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
                 <ul class="dropdown-menu settings-menu dropdown-menu-right">
